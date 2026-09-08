709637285e8be0d93b4c703bc4c6fed85c971ff9
Beomseok Kim <bskkim2022@gmail.com>
2026-09-09 07:54:14 +0900

    chore: export complete detector API

--- lss/models/__init__.py
+++ lss/models/__init__.py
@@ -1,4 +1,5 @@
 from .head import BEVHead
 from .lss import LSS
+from .detector import LSSDetector
 
-__all__ = ["BEVHead", "LSS"]
+__all__ = ["BEVHead", "LSS", "LSSDetector"]
---
+++ tests/test_detector_export.py
@@ -0,0 +1,2 @@
+from lss.models import LSSDetector
+def test_detector_is_publicly_exported(): assert LSSDetector is not None
